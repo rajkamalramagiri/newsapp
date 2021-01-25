acc53092cffbcbfdcaeef5d4cba6ce9e80f56db6
Rajkamal <rajkamal.smit@gmail.com>
2021-01-25 15:57:19 +0530

    added default tabs from native-base

---
+++ Newsapp/Screens/tabs/tab2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+function tab2() {
+  return (
+    <View>
+      <Text>tab1</Text>
+    </View>
+  );
+}
+
+export default tab2;
