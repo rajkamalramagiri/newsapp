203cdce8628a8170986ede18e621df65f3c861a8
Rajkamal <rajkamal.smit@gmail.com>
2021-01-25 16:47:13 +0530

    ui done for tabl1

--- Newsapp/Screens/TabScreen.js
+++ Newsapp/Screens/TabScreen.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
-import {Container, Header, Content, Tab, Tabs} from 'native-base';
+import {
+  Container,
+  Header,
+  Content,
+  Tab,
+  Tabs,
+  Left,
+  Body,
+  Title,
+} from 'native-base';
 import Tab1 from './tabs/tab1';
 import Tab2 from './tabs/tab2';
 import Tab3 from './tabs/tab3';
@@ -7,15 +16,33 @@ export default class TabsExample extends Component {
   render() {
     return (
       <Container>
-        <Header hasTabs />
+        <Header style={{backgroundColor: '#009387'}} hasTabs>
+          <Left>
+            <Body>
+              <Title style={{color: 'white'}}>News App</Title>
+            </Body>
+          </Left>
+        </Header>
         <Tabs>
-          <Tab heading="Tab1">
+          <Tab
+            tabStyle={{backgroundColor: '#009387'}}
+            activeTabStyle={{backgroundColor: '#009387'}}
+            textStyle={{color: 'white'}}
+            heading="Tab1">
             <Tab1 />
           </Tab>
-          <Tab heading="Tab2">
+          <Tab
+            tabStyle={{backgroundColor: '#009387'}}
+            activeTabStyle={{backgroundColor: '#009387'}}
+            textStyle={{color: 'white'}}
+            heading="Tab2">
             <Tab2 />
           </Tab>
-          <Tab heading="Tab3">
+          <Tab
+            tabStyle={{backgroundColor: '#009387'}}
+            activeTabStyle={{backgroundColor: '#009387'}}
+            textStyle={{color: 'white'}}
+            heading="Tab3">
             <Tab3 />
           </Tab>
         </Tabs>
--- Newsapp/Screens/tabs/tab1.js
+++ Newsapp/Screens/tabs/tab1.js
@@ -1,11 +1,96 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-function tab1() {
-  return (
-    <View>
-      <Text>tab1</Text>
-    </View>
-  );
-}
+import React, {Component} from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+} from 'native-base';
+export default class ListThumbnailExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail
+                  square
+                  source={{
+                    uri:
+                      'https://images.newindianexpress.com/uploads/user/imagelibrary/2020/1/21/w1200X800/Sachin_Tendulkar_AFP.jpg',
+                  }}
+                />
+              </Left>
+              <Body>
+                <Text>Sachin Tendulkar</Text>
+                <Text note numberOfLines={2}>
+                  Its time to build a difference . . Its time to build a
+                  difference . .
+                </Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
 
-export default tab1;
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail
+                  square
+                  source={{
+                    uri:
+                      'https://images.newindianexpress.com/uploads/user/imagelibrary/2020/1/21/w1200X800/Sachin_Tendulkar_AFP.jpg',
+                  }}
+                />
+              </Left>
+              <Body>
+                <Text>Sachin Tendulkar</Text>
+                <Text note numberOfLines={2}>
+                  Its time to build a difference . . Its time to build a
+                  difference . .
+                </Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail
+                  square
+                  source={{
+                    uri:
+                      'https://images.newindianexpress.com/uploads/user/imagelibrary/2020/1/21/w1200X800/Sachin_Tendulkar_AFP.jpg',
+                  }}
+                />
+              </Left>
+              <Body>
+                <Text>Sachin Tendulkar</Text>
+                <Text note numberOfLines={2}>
+                  Its time to build a difference . . Its time to build a
+                  difference . .
+                </Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
